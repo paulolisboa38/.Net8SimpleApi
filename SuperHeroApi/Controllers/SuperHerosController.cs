947f70495654a4f0595e0510e11a729bb3bf54d2
paulolisboa38 <paulolisboa38@gmail.com>
2023-11-29 04:53:57 -0300

    Add: Implement GetAllHeroes Endpoint in SuperHerosController

---
+++ SuperHeroApi/Controllers/SuperHerosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroApi.Entities;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHerosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<SuperHero>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new() {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+            return Ok(heroes);
+        }
+    }
+}
