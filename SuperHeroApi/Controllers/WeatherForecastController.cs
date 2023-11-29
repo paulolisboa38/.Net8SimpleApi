c4a0a4b41d8d3a6a35b90d11a4e362e60bb6d820
paulolisboa38 <paulolisboa38@gmail.com>
2023-11-29 04:29:02 -0300

    Refactor: Improve Readability of Summaries Array in WeatherForecastController

--- SuperHeroApi/Controllers/WeatherForecastController.cs
+++ SuperHeroApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,8 @@ namespace SuperHeroApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm",
+            "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
