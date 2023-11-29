d53af5de8ecf8c5a06e3fe9d6a2eba31246fd5d8
paulolisboa38 <paulolisboa38@gmail.com>
2023-11-29 05:21:31 -0300

    Add: Configure EntityFramework SQL Server and Setup DataContext

---
+++ SuperHeroApi/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Entities;
+
+namespace SuperHeroApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
--- SuperHeroApi/Program.cs
+++ SuperHeroApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Data;
+
 namespace SuperHeroApi
 {
     public class Program
@@ -13,6 +16,11 @@ namespace SuperHeroApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
