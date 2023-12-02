3b21eabe83bdabcc4575732ea3cacaa333c126d6
Paulo Lisboa <paulolisboa38@gmail.com>
2023-12-02 03:52:57 -0300

    Add: SuperHeroDto class

---
+++ SuperHeroApi/Dtos/SuperHeroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroApi.Entities
+{
+    public class SuperHeroDto
+    {
+        public required string Name { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
